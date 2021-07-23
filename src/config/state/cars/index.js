6aa43f95379a5c6beb4e7d280f42ce9dbdbb9fd3
hassan mokhtar <zakariahassan1995@gmail.com>
2021-07-23 16:57:47 +0200

    refactor reducers and rewrite Readme file

--- src/config/state/cars/index.js
+++ src/config/state/cars/index.js
@@ -1,10 +1,32 @@
-import { combineReducers } from "redux";
-import makesReducer from "./makesReducer";
-import modelsReducer from "./modelsReducer";
-import selectedCarsReducer from "./selectedCarsReducer";
+import Types from "./types";
+import { combineReducers } from 'redux';
+const INITIAL_STATE = {
+    data: [],
+    loaded: false,
+    errors: false,
+};
+
+function generateGetListReducer(listName) {
+    return function listReducer(state = INITIAL_STATE, action) {
+        switch (action.type) {
+            case Types[`GET_${listName}_SUCCESS`]:
+              return { data: action.payload, errors: false, loaded: true };
+            case Types[`GET_${listName}_REQUEST`]:
+              return { ...state, loaded: false };
+            case Types[`GET_${listName}_ERROR`]:
+              return { ...state, loaded: true, errors: action.payload };
+            case Types.CLEAN:
+            return INITIAL_STATE;  
+            default:
+              return state;
+          }
+    }
+}
+
 
 export default combineReducers({
-  makes: makesReducer,
-  models: modelsReducer,
-  selectedCars: selectedCarsReducer,
+    makes: generateGetListReducer("CARS_MAKES"),
+    models: generateGetListReducer("CARS_MODELS"),
+    selectedCars: generateGetListReducer("SELECTED_CARS"),
 });
+
